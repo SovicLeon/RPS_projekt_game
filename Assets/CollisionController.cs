659d7d7881f503a52b731fec0a169b4005eb04cc
Martin Gruber <89796190+NotMort@users.noreply.github.com>
2023-04-25 20:45:01 +0200

    Revert "UI Health"
    This reverts commit adcab56af808074a43a2e0d4db2da04f73bbc3b4.

--- Assets/CollisionController.cs
+++ Assets/CollisionController.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class CollisionController : MonoBehaviour
 {
   public GameObject player;
+   private void Start() {
+    player = GetComponent<GameObject>();
+  }
   private void OnCollisionEnter2D(Collision2D obj) {
-    if(obj.gameObject.CompareTag("Meteors")){
+    if(obj.gameObject.CompareTag("Meteor")){
         player.GetComponent<PlayerHealth>().TakeDamage(100);
     }
      if(obj.gameObject.CompareTag("Enemy")){
-        player.GetComponent<PlayerHealth>().TakeDamage(10);
+        
     }
   }
   private void OnTriggerEnter2D(Collider2D obj) {
