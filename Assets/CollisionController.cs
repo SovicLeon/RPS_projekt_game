60cfe89aa00417b37342802c1c73d84055ac2513
ericQronaveter <eric.mrbadass@gmail.com>
2023-05-16 16:45:28 +0200

    Implemented damage from projectiles for both player and enemy objects

--- Assets/CollisionController.cs
+++ Assets/CollisionController.cs
@@ -13,25 +13,39 @@ public class CollisionController : MonoBehaviour
      }
        if(obj.gameObject.CompareTag("Enemy"))
          ObjectOfInt.GetComponent<PlayerHealth>().TakeDamage(10);
-    } 
-    
+    }
     else if(ObjectOfInt.gameObject.CompareTag("Enemy")){
       if(obj.gameObject.CompareTag("Player"))
        ObjectOfInt.GetComponent<EnemyHealth>().TakeDamage(10);
        Debug.Log("Hit");
     }
   }
-  private void OnTriggerEnter2D(Collider2D obj) {
-    if(ObjectOfInt.gameObject.CompareTag("Player")){
-     if(obj.gameObject.CompareTag("MovementSpeed")){
-          Destroy(obj.gameObject);
-              ObjectOfInt.GetComponent<PlayerMove>().ChangeSpeed(5f); 
-      }
-      else if (obj.gameObject.CompareTag("FireRate"))
-      {
-          Destroy(obj.gameObject);
-              projectile.GetComponent<ProjectileWeapon>().ChangeSpeed(0.5f);
-      }
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (ObjectOfInt.gameObject.CompareTag("Player"))
+        {
+            if (obj.gameObject.CompareTag("MovementSpeed"))
+            {
+                Destroy(obj.gameObject);
+                ObjectOfInt.GetComponent<PlayerMove>().ChangeSpeed(5f);
+            }
+            else if (obj.gameObject.CompareTag("FireRate"))
+            {
+                Destroy(obj.gameObject);
+                projectile.GetComponent<ProjectileWeapon>().ChangeSpeed(0.5f);
+            }
+            else if (obj.gameObject.CompareTag("EnemyShot"))
+            {
+                ObjectOfInt.GetComponent<PlayerHealth>().TakeDamage(10);
+            }
+            else if (obj.gameObject.CompareTag("Meteors"))
+            {
+                ObjectOfInt.GetComponent<PlayerHealth>().TakeDamage(100);
+            }
+        }
+        if (ObjectOfInt.gameObject.CompareTag("Enemy") && obj.gameObject.CompareTag("PlayerShot"))
+        {
+            ObjectOfInt.GetComponent<EnemyHealth>().TakeDamage(10);
+        }
     }
-  }
 }
