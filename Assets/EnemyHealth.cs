afc869e4810e6dcc456fa5029f59a5b24049df91
SovicLeon <sovicleon@gmail.com>
2023-05-23 19:47:58 +0200

    When player or enemy is hit they flash a different color for a split second

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -6,24 +6,38 @@ using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    
-     public static event Action OnEnemyDeath;
+    public static event Action OnEnemyDeath;
     public float maxHealth =20;
     private float health;
-
+    private SpriteRenderer spriteRenderer;
     
     void Start()
     {
-        health=maxHealth;
+        health = maxHealth;
+        spriteRenderer = transform.Find("enemy_ufo_beam_0").GetComponent<SpriteRenderer>();
     }
    
-    public void TakeDamage(float damage){
-         health -= damage;
-            if(health <= 0){
-            health=0;
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            health = 0;
             Destroy(this.gameObject);
             OnEnemyDeath?.Invoke();
-            
         }
+        else
+        {
+            StartCoroutine(DamageFlash(0.2f));
+        }
+    }
+
+    private IEnumerator DamageFlash(float flashDuration)
+    {
+        Color originalColor = spriteRenderer.color;
+        // A lighter shade of red
+        spriteRenderer.color = new Color(1f, 0.5f, 0.5f);
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = originalColor;
     }
 }
--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -3,25 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
+
 public class PlayerHealth : MonoBehaviour
 {
     public static event Action OnPlayerDeath;
     public float health,maxHealth =100;
     public PlayerHealthBar healthBar;
+
+    private SpriteRenderer spriteRenderer;
+
     void Start()
     {
         health=maxHealth;
         healthBar.setMaxHealth(health);
+
+        // Fetch the SpriteRenderer from the child object
+        spriteRenderer = transform.Find("player_spaceship_moving_0").GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null)
+        {
+            Debug.LogError("Failed to find SpriteRenderer on child object.");
+        }
     }
 
-    public void TakeDamage(float damage){
-        health-=damage;
+    private IEnumerator DamageFlash(float flashDuration)
+    {
+        Color originalColor = spriteRenderer.color;
+        spriteRenderer.color = new Color(originalColor.r, originalColor.g, originalColor.b, 0.5f);
+        yield return new WaitForSeconds(flashDuration);
+        spriteRenderer.color = originalColor;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
         healthBar.setHealth(health);
-        if(health<=0){
+        
+        if (spriteRenderer != null)
+        {
+            StartCoroutine(DamageFlash(0.1f));
+        }
+
+        if(health <= 0)
+        {
             health=0;
-            Debug.Log("You re dead");
+            Debug.Log("You're dead");
             OnPlayerDeath?.Invoke();
-            
         }
     }
 
@@ -41,7 +67,5 @@ public class PlayerHealth : MonoBehaviour
             health += newHealth;   
         }
         healthBar.setHealth(health);
-
     }
-
 }
